4656b941e00e53e1c6ba4ad3b93fe8b9bff6446b
Eugene Litvin <eugene.litvin@interactiv4.com>
2023-11-27 17:33:39 +0200

    Example of usage and minor fixes

---
+++ examples/load_attribute_collection.py
@@ -0,0 +1,17 @@
+from attribute.attribute_repository import AttributeRepository
+
+attribute_repository = AttributeRepository()
+
+attribute_data = {
+    "name": "sample_attribute",
+    "attribute_type": "varchar"
+}
+
+attribute = attribute_repository.create(attribute_data)
+attribute_repository.save(attribute)
+
+attribute = attribute_repository.get_by_name("sample_attribute")
+
+print(attribute.to_dict())
+
+attribute_repository.delete(attribute.attribute_id)
